2ffce2d435e379b79e4aafa839db4167ff97fb9a
Cube <wieszalajakub@gmail.com>
2025-01-13 21:04:12 +0100

    first idea of working with zips while exporting (not working one method)

--- QuizHub.Application/Modules/QuestionBase/Endpoints/Export/ExportQuestionBaseEndpoint.cs
+++ QuizHub.Application/Modules/QuestionBase/Endpoints/Export/ExportQuestionBaseEndpoint.cs
@@ -3,6 +3,8 @@ using Newtonsoft.Json;
 using QuizHub.Application.Common.Extensions;
 using QuizHub.Application.Common.Interfaces;
 using QuizHub.Application.Modules.Question.Models;
+using QuizHub.Domain.Entities;
+using System.IO.Compression;
 
 namespace QuizHub.Application.Modules.QuestionBase.Endpoints.Export;
 
@@ -34,8 +36,74 @@ public class ExportQuestionBaseEndpoint(IAppDbContext context) : Endpoint<Export
         }
 
         var questionBaseDto = ConvertToQuestionBaseDto(questionBase);
-        var serializedQuestionBase = JsonConvert.SerializeObject(questionBaseDto);
+
+        var imagesDb = await GetAllImagesFromQuestionBaseDb(questionBase, ct);
+
+        using (var memoryStream = new MemoryStream())
+        {
+            using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, leaveOpen: true))
+            {
+                string jsonFileName = $"data.json";
+                var serializedQuestionBase = JsonConvert.SerializeObject(questionBaseDto);
+
+                var jsonEntry = archive.CreateEntry(jsonFileName);
+
+                using (var writer = new StreamWriter(jsonEntry.Open()))
+                {
+                    writer.Write(serializedQuestionBase);
+                }
+
+                string imagesFolder = "images/";
+
+
+                foreach (var image in imagesDb)
+                {
+                    var imageExtension = image.ContentType.Split('/')[1];
+
+                    var imageEntry = archive.CreateEntry($"{imagesFolder}{image.Id}.{imageExtension}");
+                    using var imageStream = imageEntry.Open();
+                    await imageStream.WriteAsync(image.Data, 0, image.Data.Length, ct);
+                }
+            }
+
+            memoryStream.Position = 0;
+
+            await SendStreamAsync(
+                memoryStream,
+                fileName: $"{questionBase.Name}.zip",
+                contentType: "application/zip",
+                cancellation: ct);
+        }
     }
+
+    private async Task<List<Domain.Entities.Image>> GetAllImagesFromQuestionBaseDb(Domain.Entities.QuestionBase questionBaseDb, CancellationToken ct)
+    {
+        //here fix
+        var answerImagesArray = await Task.WhenAll(questionBaseDb.Questions
+            .SelectMany(question => question.Answers)
+            .Select(async answer =>
+                await _context.Images.FindAsync([answer.ImageId], ct)
+            )
+        );
+
+        var answerImages = answerImagesArray.Where(i => i != null).ToList();
+
+        var questions = questionBaseDb.Questions;
+
+        var questionImagesArray = await Task.WhenAll(questions
+            .Where(question => question.ImageId != null)
+            .Select(async question => await _context.Images.FindAsync([question.ImageId], ct)));
+
+        var questionImages = questionImagesArray.Where(i => i != null).ToList();
+
+        List<Domain.Entities.Image> questionBaseImages = [];
+
+        questionBaseImages.AddRange(answerImages!);
+        questionBaseImages.AddRange(questionImages!);
+
+        return questionBaseImages;
+    } 
+
     private FileManagementQuestionBaseDto ConvertToQuestionBaseDto(Domain.Entities.QuestionBase questionBaseDb)
     {
         var questions = questionBaseDb.Questions
@@ -57,7 +125,7 @@ public class ExportQuestionBaseEndpoint(IAppDbContext context) : Endpoint<Export
         };
     }
 
-    private FileManagementAnswerDto ConvertToAnswerDto(Domain.Entities.Answer answerDb)
+    private FileManagementAnswerDto ConvertToAnswerDto(Answer answerDb)
     {
         return new FileManagementAnswerDto
         {
