22408909221f6d3bf391cb1b844fdcf421a9822c
Cube <wieszalajakub@gmail.com>
2025-01-26 19:09:04 +0100

    get tests endpoint

---
+++ QuizHub.Application/Modules/TestCreator/Endpoints/Get/GetTestsEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizHub.Application.Common.Extensions;
+using QuizHub.Application.Common.Interfaces;
+using QuizHub.Application.Common.Models;
+using QuizHub.Application.Modules.TestCreator.Models;
+
+namespace QuizHub.Application.Modules.TestCreator.Endpoints.Get;
+
+public class GetTestsEndpoint(IAppDbContext context) : EndpointWithoutRequest<Result<GetTestsResponse>>
+{
+    private readonly IAppDbContext _context = context;
+
+    public override void Configure()
+    {
+        Get("test");
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var userId = User.GetId();
+
+        var userTests = await _context.Tests
+            .Where(test => test.OwnerId == userId)
+            .ToListAsync(ct);
+
+        if (userTests.Count == 0)
+        {
+            await SendOkAsync(Result<GetTestsResponse>.Success(new([])), ct);
+            return;
+        }
+
+        List<TestData> testDatas = userTests
+            .Select(test => new TestData(test.Id, test.Name, test.Code, test.IsActive))
+            .ToList();
+
+        await SendOkAsync(Result<GetTestsResponse>.Success(new(testDatas)), ct);
+    }
+}
---
+++ QuizHub.Application/Modules/TestCreator/Models/TestData.cs
@@ -0,0 +1,3 @@
+﻿namespace QuizHub.Application.Modules.TestCreator.Models;
+
+public record TestData(Guid Id, string Name, string Code, bool IsActive);
