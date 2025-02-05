95894b472d9f9ccceceb9bb497d5009c0fbbfc6a
Cube <wieszalajakub@gmail.com>
2025-02-05 21:13:48 +0100

    now beginning of finish

--- QuizHub.Application/Modules/Client/Endpoints/Finish/FinishTestEndpoint.cs
+++ QuizHub.Application/Modules/Client/Endpoints/Finish/FinishTestEndpoint.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using QuizHub.Application.Common.Interfaces;
+using QuizHub.Application.Common.Models;
 
-namespace QuizHub.Application.Modules.Client.Endpoints.Finish
+namespace QuizHub.Application.Modules.Client.Endpoints.Finish;
+
+public class FinishTestEndpoint(IAppDbContext context) : Endpoint<FinishTestRequest, Result<FinishTestResponse>>
 {
-    internal class FinishTestEndpoint
+    private readonly IAppDbContext _context = context;
+
+    public override void Configure()
     {
+        Post("client/finish");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(FinishTestRequest req, CancellationToken ct)
+    {
+        var testSolvingDb = await _context.TestSolvings.FirstOrDefaultAsync(ts => ts.Id == req.TestSolvingId, ct);
+
+        if (testSolvingDb == null)
+        {
+            await SendOkAsync(Result<FinishTestResponse>.Error("Wystąpił nieoczekiwany błąd"), ct);
+            return;
+        }
+
+        if (!testSolvingDb.QuestionsDownloaded)
+        {
+            await SendOkAsync(Result<FinishTestResponse>.Error("Wystąpił nieoczekiwany błąd"), ct);
+            return;
+        }
+
+
     }
 }
