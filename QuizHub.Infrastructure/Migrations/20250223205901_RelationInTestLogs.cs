1cd9100ef087a321f958e2c4457fe41719918003
Cube <wieszalajakub@gmail.com>
2025-02-24 00:56:46 +0100

    getTestHistories endpoint, added relation for db performance

---
+++ QuizHub.Infrastructure/Migrations/20250223205901_RelationInTestLogs.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationInTestLogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_TestLogs_TestId",
+                table: "TestLogs",
+                column: "TestId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestLogs_Tests_TestId",
+                table: "TestLogs",
+                column: "TestId",
+                principalTable: "Tests",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestLogs_Tests_TestId",
+                table: "TestLogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestLogs_TestId",
+                table: "TestLogs");
+        }
+    }
+}
