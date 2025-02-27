46bcbf3b53c73708418af61ddf1c9e06fe8fd840
Cube <wieszalajakub@gmail.com>
2025-02-27 16:53:35 +0100

    fix

--- QuizHub/src/app/common/global-functions.ts
+++ QuizHub/src/app/common/global-functions.ts
@@ -3,7 +3,11 @@ export function convertTimeInSecondsToTimeString(totalSeconds: number): string {
   const minutes = Math.floor(totalSeconds / 60) % 60;
   const hours = Math.floor(totalSeconds / 3600);
 
-  return `${hours.toString().padStart(2, '0')}:
-    ${minutes.toString().padStart(2, '0')}:
-    ${seconds.toString().padStart(2, '0')}`;
+  return (
+    hours.toString().padStart(2, '0') +
+    ':' +
+    minutes.toString().padStart(2, '0') +
+    ':' +
+    seconds.toString().padStart(2, '0')
+  );
 }
