84a940af312d0041bb208514f25abc4a163e19a0
Cube <wieszalajakub@gmail.com>
2025-02-28 16:27:18 +0100

    selected answers getter connected

--- QuizHub/src/app/manager/services/test-logs.service.ts
+++ QuizHub/src/app/manager/services/test-logs.service.ts
@@ -1,8 +1,12 @@
 import { inject, Injectable } from '@angular/core';
 import { TestLog, TestLogData } from '../models/testLog';
-import { SelectedAnswersData } from '../models/selectedAnswersData';
+import {
+  SelectedAnswer,
+  SelectedAnswersData,
+  UsedQuestion,
+} from '../models/selectedAnswersData';
 import { QuestionType } from '../../common/enums/questionType';
-import { Observable } from 'rxjs';
+import { forkJoin, map, Observable, of, switchMap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
 import { ToastService } from '../../common/services/toast.service';
@@ -11,6 +15,23 @@ import {
   Result,
 } from '../../common/models/result';
 import { PaginatedData } from '../../common/models/paginatedData';
+import { ImageService } from '../../common/services/image.service';
+
+type SelectedAnswerWithImageId = Omit<SelectedAnswer, 'image'> & {
+  imageId: string | null;
+};
+
+type UsedQuestionWithImageId = Omit<Omit<UsedQuestion, 'image'>, 'answers'> & {
+  imageId: string | null;
+  answers: SelectedAnswerWithImageId[];
+};
+
+type GetSelectedAnswersDataResponse = Omit<
+  SelectedAnswersData,
+  'usedQuestions'
+> & {
+  usedQuestions: (UsedQuestionWithImageId | null)[];
+};
 
 @Injectable({
   providedIn: 'root',
@@ -18,6 +39,7 @@ import { PaginatedData } from '../../common/models/paginatedData';
 export class TestLogsService {
   private readonly http = inject(HttpClient);
   private readonly toastService = inject(ToastService);
+  private readonly imageService = inject(ImageService);
 
   private readonly apiUrl = environment.apiUrl;
 
@@ -25,7 +47,15 @@ export class TestLogsService {
     this.toastService.displayToast('error', 'Błąd', detail);
   }
 
-  getSelectedAnswersData(testLogId: string): Observable<SelectedAnswersData> {}
+  getSelectedAnswersData(
+    testLogId: string
+  ): Observable<SelectedAnswersData | null> {
+    return this.http
+      .get<Result<GetSelectedAnswersDataResponse>>(
+        this.apiUrl + '/selected-answers/' + testLogId
+      )
+      .pipe(this.handleSelectedAnswersResultPatternResponse());
+  }
 
   getTestLogData(
     testId: string,
@@ -72,5 +102,80 @@ export class TestLogsService {
           (detail) => this.displayErrorToast(detail)
         )
       );
+  }
+
+  private handleSelectedAnswersResultPatternResponse() {
+    return (
+      source: Observable<Result<GetSelectedAnswersDataResponse>>
+    ): Observable<SelectedAnswersData | null> =>
+      source.pipe(
+        switchMap((result: Result<GetSelectedAnswersDataResponse>) => {
+          if (!result.isSuccess) {
+            this.displayErrorToast(result.message || 'Wystąpił błąd');
+            return of(null);
+          }
+
+          const data = result.value;
+
+          return this.mapGetSelectedAnswersResponseToData(
+            data.usedQuestions
+          ).pipe(
+            map((questions: (UsedQuestion | null)[]) => ({
+              usedQuestions: questions,
+              testId: data.testId,
+              solveDate: data.solveDate,
+              durationInSeconds: data.durationInSeconds,
+              username: data.username,
+              earnedPoints: data.earnedPoints,
+              maxPoints: data.maxPoints,
+            }))
+          );
+        })
+      );
+  }
+
+  private mapGetSelectedAnswersResponseToData(
+    usedQuestionList: (UsedQuestionWithImageId | null)[]
+  ): Observable<(UsedQuestion | null)[]> {
+    const mappedQuestions$ = usedQuestionList.map((uq) => {
+      if (uq == null) {
+        return of(null);
+      }
+
+      const questionImage$ = uq.imageId
+        ? this.imageService.getImageFromApi(uq.imageId)
+        : of(null);
+
+      const answers$ = this.buildAnswersForUsedQuestions(uq);
+
+      return forkJoin([questionImage$, forkJoin(answers$)]).pipe(
+        map(([questionImage, answers]) => ({
+          content: uq.content,
+          questionType: uq.questionType,
+          image: questionImage,
+          isScored: uq.isScored,
+          answers,
+        }))
+      );
+    });
+
+    return forkJoin(mappedQuestions$);
+  }
+
+  private buildAnswersForUsedQuestions(uq: UsedQuestionWithImageId) {
+    return uq.answers.map((answer) => {
+      const answerImage$ = answer.imageId
+        ? this.imageService.getImageFromApi(answer.imageId)
+        : of(null);
+
+      return answerImage$.pipe(
+        map((answerImage) => ({
+          content: answer.content,
+          isCorrect: answer.isCorrect,
+          image: answerImage,
+          isSelected: answer.isSelected,
+        }))
+      );
+    });
   }
 }
